856c985639417a393f4dc016acc41af438bc24fd
Mmerichukwu Anosike <67147759+Roarnotes@users.noreply.github.com>
2022-08-10 00:08:39 +0100

    lawyers update

--- src/pages/get-a-lawyer/get-a-lawyer.component.jsx
+++ src/pages/get-a-lawyer/get-a-lawyer.component.jsx
@@ -2,23 +2,31 @@ import { Sidebar } from "../../components/navigation/sidebar.component";
 import { UserInfo } from "../../components/user-info/user-info.component";
 
 const lawyers = [
-    { 
-        name: 'Mmerichukwu Anosike', 
-        rating: 4.2,
-    },
-    { 
-        name: 'Glory Amadife', 
-        rating: 4.5,
-    },
-    { 
-        name: 'Jame Okoye', 
-        rating: 4.6,
-    },
-    { 
-        name: 'Michael Okonkwo', 
-        rating: 4.2,
-    },
-]
+  {
+    name: "Atty. Mmerichukwu Anosike",
+    dis: "A business lawyer with 10 years of experience and 50 successful cases ",
+    img: "https://ballox-by-team-wonder.github.io/iSwear-main/assets/branding/team%20(3).jpg",
+    rating: 4.2,
+  },
+  {
+    name: "Esq. Glory Amadife",
+    dis: "A Criminal case lawyer with 15 years of experience and 100 successful cases",
+    img: "https://ballox-by-team-wonder.github.io/iSwear-main/assets/branding/team%20(4).jpg",
+    rating: 4.5,
+  },
+  {
+    name: "Esq. Jame Okoye",
+    dis: "A business lawyer with 10 years of experience and 65 successful cases",
+    img: "https://ballox-by-team-wonder.github.io/iSwear-main/assets/branding/team%20(1).jpg",
+    rating: 4.1,
+  },
+  {
+    name: "Atty. Michael Okonkwo",
+    dis: "A Cooperate lawyer with 5 years of experience and 25 successful cases",
+    img: "https://ballox-by-team-wonder.github.io/iSwear-main/assets/branding/team%20(2).jpg",
+    rating: 4.2,
+  },
+];
 
 function GetALawyer() {
   return (
@@ -45,11 +53,11 @@ function GetALawyer() {
                     <div className="card mb-4">
                       <div className="card-body">
                         <div className="text-center">
-                          <img src="https://ballox-by-team-wonder.github.io/iSwear-main/assets/branding/team%20(3).jpg" className="rounded-circle avatar-xl mb-3" alt="" />
+                          <img src={lawyer.img} className="rounded-circle avatar-xl mb-3" alt="" />
                           <h4 className="mb-0">{lawyer.name}</h4>
-                          {/* <p className="mb-0 fs-6 text-muted">
-                                    Engineering Architect
-                                </p> */}
+                          <p className="mb-0 fs-6 text-muted">
+                                   {lawyer.dis}
+                                </p> 
                         </div>
                         <div className="d-flex justify-content-between border-bottom border-top py-2 mt-3">
                           <span>Rating</span>
